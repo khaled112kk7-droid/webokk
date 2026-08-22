313725e1c64a55d722d29cbe46f4e73cf81a122b
khaled112kk7-droid <khaled112kk7@gmail.com>
2026-08-22 13:22:31 +0300

    Update CHECKCAT.py

--- CHECKCAT.py
+++ CHECKCAT.py
@@ -155,7 +155,7 @@ async def run_monitor():
                 hilal_btn = page.locator("button:has(p:has-text('الهلال'))").first
                 
                 if not await hilal_btn.is_visible(timeout=5000):
-                    hilal_btn = page.locator("button[data-testid*='ui_toggle_favorite_team']").nth(1)
+                    hilal_btn = page.locator("button[data-testid*='ui_toggle_favorite_team_651fdc90492867952e046ae2']").nth(1)
 
                 await hilal_btn.wait_for(state="visible", timeout=20000)
                 await hilal_btn.click(force=True)
